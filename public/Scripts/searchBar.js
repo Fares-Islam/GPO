d14e0f688db83f12f74f44f0b2efb55c6fcabd79
Fares Islam <rgfrfrdgfref@gmail.com>
2025-11-18 13:56:43 +0200

    Uploaded Website

---
+++ public/Scripts/searchBar.js
@@ -0,0 +1,22 @@
+let searchBar = document.getElementById("ProductSearchBar");
+let grid = document.getElementsByTagName("grid")[0];
+
+searchBar.addEventListener("input", (eventData) => {
+
+  for (let i = 0; i < grid.children.length; i++) {
+    const itemIncludesSearchValue = grid.children[i].children
+      .item(2)
+      .textContent.toLowerCase().includes(String(eventData.target.value).toLowerCase());
+    const elementContainsHideClass =
+      grid.children[i].classList.contains("hide");
+
+    // console.log(grid.children[i].children.item(2).textContent);
+    // console.log(String(eventData.target.value).toLowerCase());
+
+    if (!itemIncludesSearchValue && !elementContainsHideClass) {
+      grid.children[i].classList.add("hide");
+    } else if (itemIncludesSearchValue && elementContainsHideClass) {
+      grid.children[i].classList.remove("hide");
+    }
+  }
+});
